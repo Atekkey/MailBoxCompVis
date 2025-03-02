3c9718da72d8fdab11eb1417e462786b02115620
Atekkey <anand@tekkey.net>
2025-03-01 22:03:44 -0600

    Working, small changes

--- MailRecog/ServerTry/backend/slack.py
+++ MailRecog/ServerTry/backend/slack.py
@@ -1,6 +1,6 @@
 import slack_sdk
 
-def writeToNamesFile(name, fname = "/Users/anandtekkey/macGitRepo/MailBoxCompVis/MailRecog/names.txt"): # Will need changing
+def writeToNamesFile(name, fname = "../../names.txt"): # Will need changing
     f = open(fname, "a")
     # Do preprocessing?
     f.write(name.upper().strip(" "))
